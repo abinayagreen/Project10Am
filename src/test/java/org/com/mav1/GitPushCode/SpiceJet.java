6ad4125e0824feb144697501fea22610d4ed3de1
abinayagreen <abinayarrr7@gmail.com>
2020-08-08 14:01:10 +0530

    Git another sample

---
+++ src/test/java/org/com/mav1/GitPushCode/SpiceJet.java
@@ -0,0 +1,18 @@
+package org.com.mav1.GitPushCode;
+
+public class SpiceJet {
+
+	
+	void display()
+	{
+	 System.out.println("Welcome to git code");
+	}
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		SpiceJet sp = new SpiceJet();
+		sp.display();
+	
+
+	}
+
+}
